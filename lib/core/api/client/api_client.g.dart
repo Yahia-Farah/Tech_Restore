b39db49a036a9cc2f0a0c2b64994737318ad8401
MouayedAmrr <moied.amr15503@gmail.com>
2025-11-30 16:24:38 +0200

    feat(delivery): resolve conflicts

--- lib/core/api/client/api_client.g.dart
+++ lib/core/api/client/api_client.g.dart
@@ -398,6 +398,39 @@ class _ApiClient implements ApiClient {
   }
 
   @override
+  Future<ShopListResponse> getShops() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<ShopListResponse>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          'admin/shops',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late ShopListResponse _value;
+    try {
+      _value = ShopListResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
   Future<GetAllOffersModel> getOffers(int page) async {
     final _extra = <String, dynamic>{'auth': true};
     final queryParameters = <String, dynamic>{r'page': page};
